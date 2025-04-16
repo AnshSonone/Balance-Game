6d89ec946f65ebefaa32a4ba8c06c3808543d21c
AnshSonone <sononeansh@gmail.com>
2025-04-16 20:16:35 +0530

    add round winner padding

--- src/component/RoundResult.jsx
+++ src/component/RoundResult.jsx
@@ -6,7 +6,7 @@ function RoundResult({data}) {
         <div className="">
             <h2 className="text-[25px] py-[5px] ">Round Result</h2>
             <h4 className="text-[25px] py-[5px]">Target (80% of avg): {data.target.toFixed(2)}</h4>
-            <h5 className="text-xl bg-yellow-200 p-4 my-4 font-bold">Winner: {data.winner}</h5>
+            <h5 className="text-xl bg-yellow-200 p-2 my-4 font-bold">Winner: {data.winner}</h5>
             <ul className="grid-container mt-4">
                 {data.choices.map(p => (
                     <p key={p.name} className="">{p.name}: <br/> Predicted : <span className="bg-black text-white p-[5px] mt-[16px]">{p.choice}</span></p>
